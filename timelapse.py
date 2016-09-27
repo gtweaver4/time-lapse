40961ba20f02ef39007fe043c423ef4dd2aab67b
Grant Weaver <gtweaver4@gmail.com>
2016-09-27 19:28:30 -0400

    updated timelapse.py
    user submitted audio now works
    had to remove radio buttons as those seemed to not work well
    with what I was trying to accomplish

--- timelapse.py
+++ timelapse.py
@@ -2,33 +2,56 @@ import sys
 from Tkinter import *
 from tkFileDialog import askopenfilename
 from moviepy.editor import *
-
-
-def clipSelection():
-    clip = VideoFileClip(path)
+import subprocess
+import os
 
 #creates the file selection gui
 def fileSelector():
     Tk().withdraw()
     return askopenfilename()
 
-#creates the cancel button
+#saves path for audio file
+def get_audio_path():
+    global audio_path 
+    audio_path = fileSelector()
+    
+#define audio path so that it can be used
+#to determine if get_audio_path has been called
+audio_path = ""
+
+#creates the cancel button's command
 def quit(root):
     root.quit()
 
-def export(clip):
+def export(path):
+    clip = VideoFileClip(path)
     time = float(entry_length.get())
     speed = clip.duration/time
-    clip_final = clip.fx(vfx.speedx, speed)
-    clip_final.write_videofile(entry_export.get())
+    #if audio, layer the clips
+    if(len(audio_path) > 0):
+        clip = clip.fx(vfx.speedx, clip.duration/time)
+        #shorten audio in command line
+        cmd = 'ffmpeg -ss 0 -t ' + entry_length.get() + ' -i ' + audio_path + ' shortenedAudio.mp3'
+        subprocess.call(cmd, shell = True)
+        audio = AudioFileClip('shortenedAudio.mp3')
+        clip = clip.set_audio(audio)
+        clip.write_videofile(entry_export.get())
+        os.remove('shortenedAudio.mp3')
+    #if no audio just combine clipse like normal
+    else:
+        clip = clip.fx(vfx.speedx, speed)
+        clip.write_videofile(entry_export.get())
+
+    sys.exit()
+    
 
 #select video file
 path = fileSelector()
-clip = VideoFileClip(path)
+
 
 #creating tkinter window
 root = Tk()
-root.title('Hyperlapse Creator')
+root.title('Timelapse Creator')
 root.geometry("400x500")
 
 #creates current working labels
@@ -36,28 +59,26 @@ current_working_label = Label(root, text = "You are currently working on file: "
 working_directory_label = Label(root, text = str(path)).grid(row = 2)
 audio_preference = Label(root, text = "Select your audio preference").grid(row = 5)
 
-#creates radio buttons to select audio preference
-radioVar = IntVar()
-radioVar.set(1)
-no_audio = Radiobutton(root, text="No Audio",variable= radioVar, value = 1, command = root.quit()).grid(row = 6)
-import_audio = Radiobutton(root, text = "Import Audio", variable = radioVar, value = 2, command = root.quit()).grid(row = 7)
-random_audio = Radiobutton(root, text = "Random Audio (from soundcloud)", variable = radioVar, value = 3, command = root.quit()).grid(row = 8)
+#creates an import button if user wished to import their audio
+import_audio_button = Button(root, text = "Import Audio", command = lambda root = root:get_audio_path()).grid(row = 6)
 
 #creating video length label
-video_length = Label(root, text = "\nYour clip is " + str(clip.duration) + " Seconds Long\n").grid(row = 10)
+video_length = Label(root, text = "\nYour clip is " + str(VideoFileClip(path).duration) + " Seconds Long\n").grid(row = 10)
 
 #user entry
 desire_length_label = Label(root, text = "Desired Length:").grid(row = 11, column = 0)
 entry_length = Entry(root, bd = 5, width = 5)
+entry_length.insert(0, "5")
 entry_length.grid(row = 12, column = 0)
 seconds_label = Label(root, text = "Seconds\n").grid(row = 13, column = 0)
 
 #export name
 file_export_name = Label(root, text = "\nExported File Name:   ").grid(row = 16, column = 0)
 entry_export = Entry(root, bd = 5, width = 20)
+entry_export.insert(0, "timelapse.mp4")
 entry_export.grid(row = 17, column = 0)
 
-export_button = Button(root, text = "Export", command = lambda clip = clip:export(clip)).grid(row = 18)
+export_button = Button(root, text = "Export", command = lambda path = path:export(path)).grid(row = 18)
 
 #cancel button works with sys.exit() to return to command line
 cancel_button = Button(root, text = "cancel", command= lambda root = root:quit(root)).grid(row = 19)
@@ -66,22 +87,5 @@ cancel_button = Button(root, text = "cancel", command= lambda root = root:quit(r
 #end of tkinter
 root.mainloop()
 
-#exits back to command line
+#exits back to command line as a 'last resort'
 sys.exit()
-
-'''
-clip = VideoFileClip(path)
-length = clip.duration
-print 'The video length is currently', length, 'seconds'
-desired_time = input('How long would you like the clip to be (in seconds): ')
-speed = length/desired_time
-print 'Your clip will be sped up', speed, 'times'
-clip = clip.fx(vfx.speedx, speed)
-
-#setting the title of the hyperlapse
-title = path.split('/')[-1]
-title = title.split('.')[0]
-title = title + "-lapse.mp4"
-
-clip.write_videofile(title, "-lapse.mp4")
-'''
